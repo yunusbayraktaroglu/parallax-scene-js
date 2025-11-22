b12ee17ef153ff82aabaa40d0c6006660a1721c4
Yunus Bayraktaroğlu <yunus.bayraktaroglu@gmail.com>
2025-11-23 00:05:09 +0300

    fix: eslint config bug

--- packages/config-eslint/src/next.ts
+++ packages/config-eslint/src/next.ts
@@ -5,7 +5,7 @@ import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import pluginNext from "@next/eslint-plugin-next";
 
-import { baseConfig } from "./base";
+import { baseConfig } from "./base.js";
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
--- packages/config-eslint/src/react-internal.ts
+++ packages/config-eslint/src/react-internal.ts
@@ -4,7 +4,7 @@ import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 
-import { baseConfig } from "./base";
+import { baseConfig } from "./base.js";
 
 /**
  * A custom ESLint configuration for libraries that use React.
